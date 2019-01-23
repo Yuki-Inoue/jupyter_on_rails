8aec8b89842e99a714314866257785c5c61cb734
Yuki INOUE <inoueyuworks@gmail.com>
2019-01-23 22:05:38 +0900

    init

---
+++ lib/jupyter_on_rails.rb
@@ -0,0 +1,6 @@
+require "jupyter_on_rails/version"
+
+module JupyterOnRails
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/jupyter_on_rails/version.rb
@@ -0,0 +1,3 @@
+module JupyterOnRails
+  VERSION = "0.1.0"
+end
