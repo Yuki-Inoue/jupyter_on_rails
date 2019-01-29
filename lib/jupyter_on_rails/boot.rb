21c3d751c42153d688cf0ab982669e390420119f
Yuki INOUE <inoueyuworks@gmail.com>
2019-01-29 22:22:08 +0900

    remove iruby_kernel_extention

--- lib/jupyter_on_rails/boot.rb
+++ lib/jupyter_on_rails/boot.rb
@@ -3,9 +3,6 @@ root = ENV.fetch('RAILS_ROOT')
 boot_file = File.expand_path('config/boot.rb', root)
 require boot_file
 
-require_relative 'iruby_kernel_extention'
-JupyterOnRails::IRubyKernelExtention.root = root
-
 require 'iruby'
 IRuby::Kernel.after_initialize do
   original = Dir.pwd
