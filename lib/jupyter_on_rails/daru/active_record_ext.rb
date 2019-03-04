09b8fb89e5537edb107dea42d86bb8dd6a9f2889
Yuki INOUE <inoueyuworks@gmail.com>
2019-03-04 22:21:46 +0900

    fix

--- lib/jupyter_on_rails/daru/active_record_ext.rb
+++ lib/jupyter_on_rails/daru/active_record_ext.rb
@@ -10,16 +10,20 @@ module JupyterOnRails
           relation = all
           loaded_associations =
             %i[includes_values preload_values eager_load_values]
-            .map(&relation.method(:send))
-            .reduce(&:union)
+            .map(&relation.method(:send)).reduce(&:|)
 
           datas = relation.flat_map do |record|
             ret = [record.attributes.symbolize_keys]
             loaded_associations.each do |assoc|
               ret = ret.flat_map do |attrs|
-                record.send(assoc).map(&:attributes).flat_map do |assoc_attrs|
-                  assoc_attrs.transform_keys { |key| "#{assoc}.#{key}".to_sym }
-                  attrs.merge(assoc_attrs)
+                assocs = record.send(assoc)
+                next [attrs] unless assocs.present?
+
+                assocs.map(&:attributes).map do |assoc_attrs|
+                  new_attrs = assoc_attrs.transform_keys do |key|
+                    "#{assoc}.#{key}".to_sym
+                  end
+                  attrs.merge(new_attrs)
                 end
               end
             end
--- lib/jupyter_on_rails/version.rb
+++ lib/jupyter_on_rails/version.rb
@@ -1,3 +1,3 @@
 module JupyterOnRails
-  VERSION = '0.4.3'
+  VERSION = '0.4.4'
 end
