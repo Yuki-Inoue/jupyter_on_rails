20f7c67f2ba1da05aaa0ff65a6d0bf5c95474089
Yuki INOUE <inoueyuworks@gmail.com>
2019-03-04 22:12:00 +0900

    try handle association

--- lib/jupyter_on_rails/daru/active_record_ext.rb
+++ lib/jupyter_on_rails/daru/active_record_ext.rb
@@ -7,7 +7,26 @@ module JupyterOnRails
 
       class_methods do
         def to_df
-          ::Daru::DataFrame.from_activerecord(all)
+          relation = all
+          loaded_associations =
+            %i[includes_values preload_values eager_load_values]
+            .map(&relation.method(:send))
+            .reduce(&:union)
+
+          datas = relation.flat_map do |record|
+            ret = [record.attributes.symbolize_keys]
+            loaded_associations.each do |assoc|
+              ret = ret.flat_map do |attrs|
+                record.send(assoc).map(&:attributes).flat_map do |assoc_attrs|
+                  assoc_attrs.transform_keys { |key| "#{assoc}.#{key}".to_sym }
+                  attrs.merge(assoc_attrs)
+                end
+              end
+            end
+            ret
+          end
+
+          ::Daru::DataFrame.from_activerecord(datas)
         end
       end
     end
