94d4708f5a0dd56498caa1f0a870cf4274486c5b
Mark Lee <Mark.Lee@infogroup.com>
2019-09-17 13:25:20 -0700

    Only require activerecord-import for Rails < 6
    Rails 6 and above have a built-in bulk insert method.

--- lib/jupyter_on_rails/daru/data_frame_ext.rb
+++ lib/jupyter_on_rails/daru/data_frame_ext.rb
@@ -2,10 +2,28 @@ module JupyterOnRails
   module Daru
     module DataFrameExt
       def write_model(model)
-        require 'activerecord-import'
+        if ActiveRecord.version >= Gem::Version.new('6.0.0.beta1')
+          write_model_rails(model)
+        else
+          write_model_activerecord_import(model)
+        end
+      end
 
-        objs = map_rows(&:to_h).map(&model.method(:new))
-        model.import(objs)
+      private
+
+      def write_model_activerecord_import(model)
+        begin
+          require 'activerecord-import'
+        rescue LoadError
+          raise 'write_model requires either Rails >= 6 or the activerecord-import gem'
+        end
+
+        records = map_rows(&:to_h).map(&model.method(:new))
+        model.import(records)
+      end
+
+      def write_model_rails(model)
+        model.insert_all(map_rows(&:to_h))
       end
     end
   end
