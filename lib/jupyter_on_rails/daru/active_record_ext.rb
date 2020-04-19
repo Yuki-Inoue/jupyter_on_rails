6736bb1eec2478bba53b62de13d4b76fd7d2c9ff
Yuki INOUE <inoueyuworks@gmail.com>
2020-04-19 23:28:40 +0900

    fix rubocop

--- lib/jupyter_on_rails/daru/active_record_ext.rb
+++ lib/jupyter_on_rails/daru/active_record_ext.rb
@@ -32,9 +32,7 @@ module JupyterOnRails
                 assocs = record.send(assoc)
                 next [attrs] unless assocs.present?
 
-                unless assocs.is_a?(ActiveRecord::Associations::CollectionProxy)
-                  assocs = [assocs]
-                end
+                assocs = [assocs] unless assocs.is_a?(ActiveRecord::Associations::CollectionProxy)
                 assocs.map(&:attributes).map do |assoc_attrs|
                   new_attrs = assoc_attrs.transform_keys do |key|
                     "#{assoc}.#{key}".to_sym
