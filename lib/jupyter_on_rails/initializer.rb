26991ffe1ae576e17a9c9c594e14cc32ce6eb13f
井上裕貴 <yuki.inoue@xincere.jp>
2022-05-20 20:28:35 +0900

    feat: include Rails::ConsoleMethods in kernel main

--- lib/jupyter_on_rails/initializer.rb
+++ lib/jupyter_on_rails/initializer.rb
@@ -1,8 +1,26 @@
 # frozen_string_literal: true
 
 module JupyterOnRails
-  module Initializer
-    def self.run(root:, sandbox:)
+  class Initializer
+    def self.run(**opts)
+      new(**opts).run
+    end
+
+    def initialize(root:, sandbox:)
+      @root = root
+      @sandbox = sandbox
+    end
+
+    attr_reader :root, :sandbox
+
+    def run
+      setup_daru
+      load_rails_environment
+      setup_console_methods
+      setup_sandbox_transaction if sandbox
+    end
+
+    def setup_daru
       require 'active_support/lazy_load_hooks'
 
       ActiveSupport.on_load(:active_record) do
@@ -33,16 +51,25 @@ module JupyterOnRails
           end
         end
       end
+    end
 
-      original = Dir.pwd
-      Dir.chdir root
+    def load_rails_environment
       app_file = File.expand_path('config/environment.rb', root)
-      require app_file
-      Rails.application.require_environment!
-      Dir.chdir original
+      Dir.chdir(root) do
+        require app_file
+      end
+    end
 
-      return unless sandbox
+    def setup_console_methods
+      require 'rails/console/app'
+      IRuby::Kernel
+        .instance
+        .instance_variable_get(:@backend)
+        .instance_variable_get(:@main)
+        .extend(Rails::ConsoleMethods)
+    end
 
+    def setup_sandbox_transaction
       ActiveRecord::Base.connection.begin_transaction(joinable: false)
       at_exit do
         ActiveRecord::Base.connection.rollback_transaction
